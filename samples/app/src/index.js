80dd28dbc9508bd6dae59b9e4fa463838efe1518
Yann Brelière <yann@gandi.net>
2016-11-24 17:50:24 +0100

    Use `negociateLocale` in the app sample

--- samples/app/src/index.js
+++ samples/app/src/index.js
@@ -1,36 +1,42 @@
 /* global document: true */
 
+import url;
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { provideTranslate, createTranslator } from '@gandi/react-translate';
+
+import { createTranslator, negociateLocale, provideTranslate } from 'gandi.translate';
 
 import App from './App';
 import './index.css';
 
-// it's up to you to detect the locale of your user
-// (by url, cookie, header X-Accept-Language, etc)
-const matches = document.location.search.match(/lang=(.*)/);
-let locale = (Array.isArray(matches) && matches.pop()) || 'en';
-
 const availableLocales = ['fr', 'en'];
-if (!availableLocales.includes(locale)) {
-  locale = 'en';
-}
+const defaultLocale = 'en';
 
-// it's up to you to define a strategy of how to retrieve your translations
+// It's up to you to detect the locale of your user
+// (by url param, cookie, X-Accept-Language header, user pref etc)
+const queryParamLocale = url.parse(redirectTo, true).query.lang;
+const userBrowserLocales = window.navigator.languages ||
+	[(window.navigator.language || window.navigator.userLanguage)];
+const locale = negociateLocale([
+      queryParamLocale,
+      ...userBrowserLocales,
+    ], availableLocales, defaultLocale);
+
+// It's up to you to define a strategy of how to retrieve your translations
 // (by a <script> tag, directly into the DOM, asynchronously by fetching an url, etc)
 const translations = require(`../locales/${locale}/root.json`);
 
-// define your translations params
+// Define your translations params
 const translatorParams = {
   translations,         // translations for an unique language
   locale,               // user's locale
 };
 
-// create translator
+// Create translator
 const translator = createTranslator(translatorParams);
 
-// provide the translator to your whole app
+// Provide the translator to your whole app
 // (technically it will set the translator into the React context, you will be
 // able to retrieve it with the withTranslator decorator).
 const TranslatedApp = provideTranslate(translator)(App);
